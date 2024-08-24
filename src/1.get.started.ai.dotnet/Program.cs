de24826756ed0706d4820fd95ffb6e6831282d0b
Ponggun <wittawat@tt-ss.net>
2024-08-24 15:42:41 +0700

    hello world

---
+++ src/1.get.started.ai.dotnet/Program.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+IChatCompletionService chatService = new OpenAIChatCompletionService("gpt-4o-mini", "Key");
+
+Console.WriteLine(await chatService.GetChatMessageContentAsync("ท้องฟ้าสีอะไร"));
