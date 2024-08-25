287fe110197c1d962ee1aac6fc98162542759c2d
Ponggun <wittawat@tt-ss.net>
2024-08-25 12:43:33 +0700

    Prepare advance kernel

---
+++ src/1.get.started.ai.dotnet.advance/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+// สร้างอินสแตนซ์ของบริการแชท OpenAI Platform โดยใช้โมเดล gpt-4o-mini และคีย์ API
+IChatCompletionService chatService = new OpenAIChatCompletionService(
+    modelId: "gpt-4o-mini",
+    apiKey: "key"
+    );
+
+// แสดงผลลัพธ์ของการถามคำถามในคอนโซล
+Console.WriteLine(await chatService.GetChatMessageContentAsync("ท้องฟ้าสีอะไร"));
