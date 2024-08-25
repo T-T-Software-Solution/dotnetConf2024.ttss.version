72bc583c36ca4a8fe12c2b9d007d0bc6e2095e95
Ponggun <wittawat@tt-ss.net>
2024-08-25 21:37:49 +0700

    Correct the code

--- src/1.get.started.ai.dotnet/Program.cs
+++ src/1.get.started.ai.dotnet/Program.cs
@@ -1,18 +1,18 @@
 ﻿using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
-//// สร้างอินสแตนซ์ของบริการแชท OpenAI Platform โดยใช้โมเดล gpt-4o-mini และคีย์ API
-// IChatCompletionService chatService = new OpenAIChatCompletionService(
-//     modelId: "gpt-4o-mini",
-//     apiKey: "key"
-//     );
-
-// สร้างอินสแตนซ์ของบริการแชท Azure OpenAI Service โดยใช้โมเดล gpt-4o-mini และคีย์ API
-IChatCompletionService chatService = new AzureOpenAIChatCompletionService(
-    deploymentName: "gpt-4o-mini",
-    endpoint: "",
+// สร้างอินสแตนซ์ของบริการแชท OpenAI Platform โดยใช้โมเดล gpt-4o-mini และคีย์ API
+IChatCompletionService chatService = new OpenAIChatCompletionService(
+    modelId: "gpt-4o-mini",
     apiKey: "key"
     );
+
+// // สร้างอินสแตนซ์ของบริการแชท Azure OpenAI Service โดยใช้โมเดล gpt-4o-mini และคีย์ API
+// IChatCompletionService chatService = new AzureOpenAIChatCompletionService(
+//     deploymentName: "gpt-4o-mini",
+//     endpoint: "",
+//     apiKey: "key"
+//     );
 
 // แสดงผลลัพธ์ของการถามคำถามในคอนโซล
 Console.WriteLine(await chatService.GetChatMessageContentAsync("ท้องฟ้าสีอะไร"));
