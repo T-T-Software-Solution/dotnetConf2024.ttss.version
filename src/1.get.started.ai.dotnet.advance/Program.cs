befb22d2f173ac5dfda198a59b12bc2a6427f953
Ponggun <wittawat@tt-ss.net>
2024-08-25 21:05:48 +0700

    เพิ่ม Advance Case

--- src/1.get.started.ai.dotnet.advance/Program.cs
+++ src/1.get.started.ai.dotnet.advance/Program.cs
@@ -1,16 +1,51 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 ServiceCollection services = new ServiceCollection();
+
 services.AddOpenAIChatCompletion(
     modelId: "gpt-4o-mini",
     apiKey: "key"
 );
+
+services.AddKernel();
+// services.AddLogging(builder =>
+// {
+//     builder.AddConsole().SetMinimumLevel(LogLevel.Trace);
+// });
+
 IServiceProvider serviceProvider = services.BuildServiceProvider();
-
 IChatCompletionService chatService = serviceProvider.GetRequiredService<IChatCompletionService>();
-ChatHistory chatHistory = new ChatHistory();
-chatHistory.AddUserMessage("ท้องฟ้าสีอะไร");
+Kernel kernel = serviceProvider.GetRequiredService<Kernel>();
 
-Console.WriteLine(await chatService.GetChatMessageContentAsync(chatHistory));
+kernel.ImportPluginFromType<Demographics>();
+PromptExecutionSettings settings = new OpenAIPromptExecutionSettings() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+
+ChatHistory chatHistory = new ChatHistory();
+while (true)
+{
+    Console.Write("กรุณาระบุความต้องการ: ");
+    chatHistory.AddUserMessage(Console.ReadLine());
+
+
+    var assistant = await chatService.GetChatMessageContentAsync(chatHistory, settings, kernel);
+    Console.WriteLine(assistant);
+    chatHistory.Add(assistant);
+}
+
+class Demographics
+{
+    [KernelFunction]
+    public int GetPersonAge(string name)
+    {
+        return name switch
+        {
+            "ไข่ดาว" => 22,
+            "ไข่เจียว" => 18,
+            _ => 30
+        };
+    }
+}
