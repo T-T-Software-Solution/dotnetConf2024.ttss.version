7f0ec7f4c10e8fd02a6029cfa5c747e082a88ea2
Ponggun <wittawat@tt-ss.net>
2024-11-17 20:01:54 +0700

    Add get config file function

--- src/4.semantickernel.gettingstart/Program.cs
+++ src/4.semantickernel.gettingstart/Program.cs
@@ -6,53 +6,72 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
-
-// Populate values from your OpenAI deployment
-var modelId = "gpt-4o-mini";
-var endpoint = "";
-var apiKey = "";
-
-// Create a kernel with Azure OpenAI chat completion
-var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
-
-// Add enterprise components
-//builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
-
-// Build the kernel
-Kernel kernel = builder.Build();
-var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-
-// Add a plugin (the LightsPlugin class is defined below)
-kernel.Plugins.AddFromType<LightsPlugin>("Lights");
-
-// Enable planning
-OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+namespace SemanticKernelGettingStarted
 {
-    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
-};
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Populate values from your OpenAI deployment
+            string modelId , endpoint, apiKey;
 
-// Create a history store the conversation
-var history = new ChatHistory();
+            ReadDataFromConfig(out modelId, out endpoint, out apiKey);
 
-// Initiate a back-and-forth chat
-string? userInput;
-do {
-    // Collect user input
-    Console.Write("User > ");
-    userInput = Console.ReadLine();
+            // Create a kernel with Azure OpenAI chat completion
+            var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
 
-    // Add user input
-    history.AddUserMessage(userInput);
+            // Add enterprise components
+            //builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
 
-    // Get the response from the AI
-    var result = await chatCompletionService.GetChatMessageContentAsync(
-        history,
-        executionSettings: openAIPromptExecutionSettings,
-        kernel: kernel);
+            // Build the kernel
+            Kernel kernel = builder.Build();
+            var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
-    // Print the results
-    Console.WriteLine("Assistant > " + result);
+            // Add a plugin (the LightsPlugin class is defined below)
+            kernel.Plugins.AddFromType<LightsPlugin>("Lights");
 
-    // Add the message from the agent to the chat history
-    history.AddMessage(result.Role, result.Content ?? string.Empty);
-} while (userInput is not null);
+            // Enable planning
+            OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+            };
+
+            // Create a history store the conversation
+            var history = new ChatHistory();
+
+            // Initiate a back-and-forth chat
+            string? userInput;
+            do
+            {
+                // Collect user input
+                Console.Write("User > ");
+                userInput = Console.ReadLine();
+
+                // Add user input
+                history.AddUserMessage(userInput);
+
+                // Get the response from the AI
+                var result = await chatCompletionService.GetChatMessageContentAsync(
+                    history,
+                    executionSettings: openAIPromptExecutionSettings,
+                    kernel: kernel);
+
+                // Print the results
+                Console.WriteLine("Assistant > " + result);
+
+                // Add the message from the agent to the chat history
+                history.AddMessage(result.Role, result.Content ?? string.Empty);
+            } while (userInput is not null);
+        }
+        private static void ReadDataFromConfig(out string chatModel, out string azureAPIEndpoint, out string azureAPIKey)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+            chatModel = configuration["AzureOpenAI:ChatModel"] ?? throw new ArgumentNullException("AzureOpenAI:ChatModel");
+            azureAPIEndpoint = configuration["AzureOpenAI:Endpoint"] ?? throw new ArgumentNullException("AzureOpenAI:Endpoint");
+            azureAPIKey = configuration["AzureOpenAI:ApiKey"] ?? throw new ArgumentNullException("AzureOpenAI:ApiKey");
+        }
+    }
+}
